372909b298428ca67fbc431c0432dba5e7932d81
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-06-26 19:58:35 +0900

    [Passed] 변수 값 복사하기 2 (88ms, 9MB)

---
+++ trail1/변수 값 복사하기 2/copying-variable-values-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 7;
+        a = b = c;
+        System.out.print(a + " " + b + " " + c);
+    }
+}
