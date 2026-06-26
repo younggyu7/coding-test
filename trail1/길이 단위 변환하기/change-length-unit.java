8ac05c7876b4f0608199612e1bbe076420a0844e
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-06-26 19:10:47 +0900

    [Passed] 길이 단위 변환하기 (99ms, 9MB)

---
+++ trail1/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        double oneFeet = 30.48;
+        double oneMile = 160934;
+        System.out.printf("9.2ft = %.1fcm", oneFeet * 9.2);
+        System.out.println();
+        System.out.printf("1.3mi = %.1fcm", oneMile * 1.3);
+    }
+}
