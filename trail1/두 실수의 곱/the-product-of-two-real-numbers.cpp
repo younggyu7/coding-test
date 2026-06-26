821d632fe1c5bf9a38937fe3cef07b3ce0b453e9
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-06-26 19:16:38 +0900

    [Passed] 두 실수의 곱 (2ms, 0MB)

---
+++ trail1/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double a = 5.26;
+    double b = 8.27;
+    cout << fixed;
+    cout.precision(3);
+    cout << a * b << endl;
+    return 0;
+}
