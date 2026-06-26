11eddde6cdbdce0c1ff16aa1539339de57937a46
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-06-26 20:56:05 +0900

    [Passed] 입력받아 출력 2 (129ms, 10MB)

---
+++ trail1/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String[] parts = s.split("-");
+        System.out.println(parts[0] + parts[1]);
+    }
+}
