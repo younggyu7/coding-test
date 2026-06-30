143f2c9471f120513a405b5f5fa8abb1d1e37461
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-06-30 20:19:27 +0900

    [Passed] Run Length 인코딩 (2ms, 0MB)

--- trail1/Run Length 인코딩/run-length-encoding.cpp
+++ trail1/Run Length 인코딩/run-length-encoding.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-string A;
-
 int main() {
+    string A;
     cin >> A;
-    int pos = 0;
+
     string newStr;
-    while(pos < A.size()) {
-        char tmp = A[pos];
-        int cnt = 1;
-        for (int j = pos + 1; j < A.size(); j++) {
-            if (A[j] == tmp) {
-                pos = j + 1;
-                cnt++;
-            }
-            else break;
+    int pos = 0;
+    int len = A.size();
+
+    while (pos < len) {
+        char current = A[pos];
+        int cnt = 0;
+
+        while (pos < len && A[pos] == current) {
+            cnt++;
+            pos++;
         }
-        if (cnt == 1) pos++;
-        newStr += (tmp + to_string(cnt));
+
+        newStr += current;
+        newStr += to_string(cnt);
     }
-    cout << newStr.size() << "\n";
+
+    cout << newStr.size() << '\n';
     cout << newStr;
+
     return 0;
 }
