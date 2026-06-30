da82939f34795c0bb76715f00f15d2a82cf2a48e
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-06-30 18:05:36 +0900

    [Passed] 문자열 역순으로 출력하기 (2ms, 0MB)

---
+++ trail1/문자열 역순으로 출력하기/print-string-in-reverse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string arr[4];
+    for (int i = 0; i < 4; i++) {
+        cin >> arr[i];
+    } 
+    for (int i = 3; i >= 0; i--) {
+        cout << arr[i] << "\n";
+    } 
+    return 0;
+}
