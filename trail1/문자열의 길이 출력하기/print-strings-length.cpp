7d4b832e02f4f1940f841c0a899dafcf3010e776
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-06-30 16:21:57 +0900

    [Passed] 문자열의 길이 출력하기 (2ms, 0MB)

---
+++ trail1/문자열의 길이 출력하기/print-strings-length.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str1;
+    string str2;
+    cin >> str1 >> str2;
+    cout << str1.length() + str2.length();
+    return 0;
+}
