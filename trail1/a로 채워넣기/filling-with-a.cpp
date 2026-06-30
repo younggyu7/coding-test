bcf3962cd39e28d54fd7b72055d06ca1a55d4cf1
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-06-30 21:24:24 +0900

    [Passed] a로 채워넣기 (2ms, 0MB)

---
+++ trail1/a로 채워넣기/filling-with-a.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+    str[1] = 'a';
+    str[str.length() - 2] = 'a';
+    cout << str;
+    return 0;
+}
