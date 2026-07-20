c8ddefc6f0b1f849c731e539ad64881cbf958288
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-07-20 19:10:23 +0900

    [Passed] 시험 통과 여부 확인하기 (124ms, 10MB)

---
+++ trail1/시험 통과 여부 확인하기/verify-test-passed.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        if (n >= 80) System.out.println("pass");
+        else {
+            System.out.println((80 - n) + " more score");
+        }
+    }
+}
