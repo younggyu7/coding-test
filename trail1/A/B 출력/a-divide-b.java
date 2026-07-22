64badadc2116c795ce079700291f32c8c9d00084
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-07-22 16:49:51 +0900

    [Passed] A/B 출력 (137ms, 10MB)

---
+++ trail1/A/B 출력/a-divide-b.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(a / b + ".");
+        a = (a - ((a / b) * b)) * 10;
+        for (int i = 0; i < 20; i++) {
+            System.out.print(a / b);
+            a = (a - ((a / b) * b)) * 10;
+        }
+    }
+}
