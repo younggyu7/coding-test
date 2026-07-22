e6e917a117e0ac8a41d6e966a51ca6b73a54e49d
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-07-22 18:24:54 +0900

    [Passed] 홀수만 더하기 (127ms, 10MB)

---
+++ trail1/홀수만 더하기/add-only-odd-numbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int sumVal = 0;
+        for (int i = 0; i < n; i++) {
+            int a = sc.nextInt();
+            if (a % 2 == 1 && a % 3 == 0) sumVal += a;
+            
+        }
+        System.out.println(sumVal);
+    }
+}
