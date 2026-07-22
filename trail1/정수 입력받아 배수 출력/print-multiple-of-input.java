9ec968e47afc0f3607f3b9e2fb37990da7795999
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-07-22 16:29:14 +0900

    [Passed] 정수 입력받아 배수 출력 (132ms, 10MB)

--- trail1/정수 입력받아 배수 출력/print-multiple-of-input.java
+++ trail1/정수 입력받아 배수 출력/print-multiple-of-input.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 1; i <= 5; i++) {
-            System.out.print(n * i + " ");
+        for (int i = n; i <= n* 5; i+= n) {
+            System.out.print(i + " ");
         }
     }
 }
