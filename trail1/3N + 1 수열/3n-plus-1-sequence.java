4076f560477c7fc47315574b698be21f20cfc4a9
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-07-22 19:31:24 +0900

    [Passed] 3N + 1 수열 (149ms, 10MB)

---
+++ trail1/3N + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        while(true) {
+            if (n == 1) break;
+            if (n % 2 == 0) n /= 2;
+            else n = n * 3 + 1;
+            cnt++;
+        }
+        System.out.println(cnt);
+    }
+}
