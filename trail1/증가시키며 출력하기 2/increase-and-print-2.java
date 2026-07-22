c3fe75680a6af8cbd837dea5bdc8d071dfef1c35
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-07-22 16:36:02 +0900

    [Passed] 증가시키며 출력하기 2 (92ms, 9MB)

---
+++ trail1/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 10;
+        while (i <= 26) {
+            System.out.print(i + " ");
+            i++;
+        }
+    }
+}
