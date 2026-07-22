d7df3349d626f000e8cf8b2108d8e3fbda25ffff
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-07-22 19:08:15 +0900

    [Passed] 1부터 곱셈하기 (125ms, 10MB)

---
+++ trail1/1부터 곱셈하기/multiple-from-one.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sumVal = 1;
+        int ans = 0;
+        for (int i = 1; i <= 100; i++) {
+            sumVal *= i;
+            if (sumVal >= n) {
+                ans = i;
+                break;
+            }
+        }
+        System.out.println(ans);
+    }
+}
