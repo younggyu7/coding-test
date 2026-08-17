52f0b3ffb8589e075e68aa5e9cc49c427a92c1d4
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-08-17 17:30:20 +0900

    [Passed] 10개 입력받기 (148ms, 11MB)

---
+++ trail1/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int cnt = 0;
+        for (int i = 0; i < 10; i++) {
+            int tmp = sc.nextInt();
+            if (tmp == 0) break;
+            sum += tmp;
+            cnt++;
+        }
+        double avg = (double) sum / cnt;
+        System.out.print(sum + " ");
+        System.out.printf("%.1f", avg);
+    }
+}
