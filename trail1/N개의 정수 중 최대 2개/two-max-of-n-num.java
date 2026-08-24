9cef23533509b8dc9627bf9e5369ef03fca553cc
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-08-24 15:53:54 +0900

    [Passed] N개의 정수 중 최대 2개 (145ms, 10MB)

---
+++ trail1/N개의 정수 중 최대 2개/two-max-of-n-num.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length / 2; i++) {
+            int tmp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = tmp;
+        }
+        System.out.println(arr[0] + " " + arr[1]);
+    }
+}
