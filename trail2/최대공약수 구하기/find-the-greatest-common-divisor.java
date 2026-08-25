d9ff9ffa5ce52d203749ae521c83dbc0e473433d
표영규 <104084656+younggyu7@users.noreply.github.com>
2026-08-25 16:06:14 +0900

    [Passed] 최대공약수 구하기 (125ms, 10MB)

---
+++ trail2/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd (b, a % b);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int ans = gcd(n, m);
+        System.out.println(ans);
+    }
+}
